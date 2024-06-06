08c0f74d11d48cc7ab07ef3592316eac038d952a
rishisrinivasan <ri21082004@gmail.com>
2024-06-06 09:18:15 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int score = 0;
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int numberOfAttempts = 0;
+            boolean hasGuessedCorrectly = false;
+            final int MAX_ATTEMPTS = 10;
+
+            System.out.println("I have generated a number between 1 and 100. Try to guess it!");
+
+            while (numberOfAttempts < MAX_ATTEMPTS && !hasGuessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                numberOfAttempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Congratulations! You guessed the number correctly.");
+                    hasGuessedCorrectly = true;
+                    score++;
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (!hasGuessedCorrectly) {
+                System.out.println("Sorry! You've used all your attempts. The number was " + numberToGuess);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            playAgain = scanner.next().equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Your total score: " + score);
+        System.out.println("Thank you for playing!");
+        scanner.close();
+    }
+}
